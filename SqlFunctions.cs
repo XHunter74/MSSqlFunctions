7b5b3446a5eb765186f65a5d86fa9b1d2fcfe505
serhiy.krasovskyy <xhunter74@gmail.com>
2025-05-07 10:25:01 +0300

    Initial commit.

---
+++ SqlFunctions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SqlServer.Server;
+
+namespace SqlIsPalindrome
+{
+    public class SqlFunctions
+    {
+        [SqlFunction]
+        public static bool IsPalindrome(string input)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+
+            while (left < right)
+            {
+                if (input[left] != input[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
